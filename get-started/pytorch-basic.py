a8a409b3b05f6dbe45003c2b29a34d66225b2b6a
CHENYI ZHANG <czhang@mac.lan>
2025-06-01 23:32:40 -0700

    add get-started

---
+++ get-started/pytorch-basic.py
@@ -0,0 +1,14 @@
+# In this file, we will get started with pytorch
+
+import torch
+
+# create a tensor with 5 elements, in 1 dimension
+x = torch.empty(5)
+print(x)
+
+# create a tensor with multi-dimension tensor
+x = torch.empty(4,4)
+x = torch.rand(4,4)
+
+
+
